19d2ebc99d11e49e474cb64b7042f35b91e03607
xiaoLam <1484611740@qq.com>
2020-12-13 16:47:22 +0800

    分类页面完成

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,107 @@
 // pages/category/category.js
+import {
+  getCategory,
+  getSubcategory,
+  getCategoryDetail
+} from '../../service/category.js'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    categories: [],
+    categoryData: {},
+    currentIndex: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this._getData()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  _getData() {
+    // 1.请求分类数据
+    this._getCategory()
   },
+  _getCategory() {
+    getCategory().then(res => {
+      console.log(res);
+      // 1.获取categories
+      const categories = res.data.data.category.list;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+      // 2.初始化每个类别的子数据
+      const categoryData = {}
+      for (let i = 0; i < categories.length; i++) {
+        categoryData[i] = {
+          subcategories: [],
+          categoryDetail: []
+          // categoryDetail: {
+          //   'pop': [],
+          //   'new': [],
+          //   'sell': []
+          // }
+        }
+      }
+      console.log(categoryData);
 
+      // 3.修改data中的数据
+      this.setData({
+        categories: categories,
+        categoryData: categoryData
+      })
+
+      // 4.请求第一个类别的数据
+      this._getSubcategory(0)
+
+      // 5.请求第一个类别的详情数据
+      this._getCategoryDetail(0)
+    })
   },
+  _getSubcategory(currentIndex) {
+    // 1.获取对应的maitkey
+    const maitkey = this.data.categories[currentIndex].maitKey;
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+    // 2.请求的数据
+    getSubcategory(maitkey).then(res => {
+      const tempCategoryData = this.data.categoryData;
+      console.log(res);
+      tempCategoryData[currentIndex].subcategories = res.data.data.list;
+      this.setData({
+        categoryData: tempCategoryData
+      })
+    })
   },
+  _getCategoryDetail(currentIndex) {
+    // 1.获取对应的miniWallKey
+    const miniWallKey = this.data.categories[currentIndex].miniWallkey;
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    // 2.请求数据类别的数据
+    this._getRealCategoryDetail(currentIndex, miniWallKey, 'pop');
+    // 因为接口中的 pop new sell 展示的数据是一样的, 所以就步请求new 和 sell中的数据了
+    // this._getRealCategoryDetail(currentIndex, miniWallKey, 'new');
+    // this._getRealCategoryDetail(currentIndex, miniWallKey, 'sell');
   },
+  _getRealCategoryDetail(index, miniWallKey, type) {
+    getCategoryDetail(miniWallKey, type).then(res => {
+      // 1.获取categoryData
+      const categoryData = this.data.categoryData;
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+      console.log(res);
+      // 2.修改数据
+      categoryData[index].categoryDetail = res.data;
 
+      // 3.修改data中的数据
+      this.setData({
+        categoryData: categoryData
+      })
+    })
   },
+  menuClick(e) {
+    // 1.改变当前的currentIndex
+    const currentIndex = e.detail.currentIndex;
+    this.setData({
+      currentIndex
+    })
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    // 2.请求对应currentIndex的数据
+    this._getSubcategory(currentIndex);
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 3.请求对应的currentIndex的详情数据
+    this._getCategoryDetail(currentIndex)
   }
 })
